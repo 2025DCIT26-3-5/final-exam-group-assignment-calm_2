8cbe8f5bee46f1fc0a43f1b246bbfb4c74e81acb
Jodel Molano <220807664+MolanoJodel@users.noreply.github.com>
2026-01-13 11:12:11 +0000

    Initial & First Changes

--- App.tsx
+++ App.tsx
@@ -2,36 +2,16 @@ import { useState } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
-// Child Component with Props
-function Greeting({ name }) {
-  return <Text style={styles.greeting}>Kamote, {name}!</Text>;
-}
-
 export default function App() {
-  // State Hook
-  const [count, setCount] = useState(0);
   const [name, setName] = useState("World");
 
   return (
     <View style={styles.container}>
-      {/* Native Feature: Controls phone's status bar */}
       <StatusBar style="light" backgroundColor="red" />
 
-      <Text style={styles.title}>React Native Demo</Text>
-
-      {/* Component with Props */}
-      <Greeting name={name} />
-
-      {/* State Display */}
-      <Text style={styles.counter}>Count: {count}</Text>
-
-      {/* Native Button */}
-      <Button title="Increment" onPress={() => setCount(count + 1)} />
-
-      <Button
-        title="Change Name"
-        onPress={() => setName(name === "World" ? "Student" : "World")}
-      />
+      <View id="Nav" style={styles.Nav}>
+        Navigation Bar
+      </View>
     </View>
   );
 }
@@ -40,24 +20,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#cbd4e6ff",
+    backgroundColor: "#84cff1",
     alignItems: "center",
     justifyContent: "center",
   },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "white",
-    marginBottom: 20,
-  },
-  greeting: {
-    fontSize: 32,
-    color: "#ec261cff",
+  Nav: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    padding: 20,
     marginBottom: 10,
-  },
-  counter: {
-    fontSize: 16,
-    color: "white",
-    marginVertical: 20,
   },
 });
